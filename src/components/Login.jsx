0d7a1585948a2b9c17d3d0e758f7ef2e666c36d4
gcarniel <gcarniel@outlook.com>
2022-10-15 13:35:42 -0300

    pagina de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    localStorage.setItem('userId', username);
+    setUsername('');
+    navigate('/tasks');
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login__container">
+      <form className="login__form" onSubmit={handleLogin}>
+        <label htmlFor="username">Informe um nome de usuário</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <button>Entrar</button>
+      </form>
     </div>
   );
 };
